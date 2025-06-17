806ada03a48507ec831fd854deed7f7b02b9f9a4
Tisha Gupta <vanig1812@gmail.com>
2025-06-17 20:53:21 +0530

    Made chnages to run it in local system

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ const listingRouter=require("./routes/listing.js");
 const reviewRouter=require("./routes/review.js");
 const userRouter=require("./routes/user.js");
 
-// const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust"
+const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust"
 
-const dbUrl=process.env.ATLASDB_URL;
+// const dbUrl=process.env.ATLASDB_URL;
 main().then(()=>{
     console.log("connected to DB ");
 })
@@ -31,8 +31,8 @@ main().then(()=>{
 });
 
 async function main(){
-    // await mongoose.connect(MONGO_URL);
-     await mongoose.connect(dbUrl);
+    await mongoose.connect(MONGO_URL);
+    //  await mongoose.connect(dbUrl);
     
 }
 
@@ -45,7 +45,8 @@ app.use(express.static(path.join(__dirname,"/public")));
 
 
 const store=MongoStore.create({
-    mongoUrl:dbUrl,
+    // mongoUrl:dbUrl,
+    mongoUrl:MONGO_URL,
     crypt:{
         secret:process.env.SECRET,
     },
